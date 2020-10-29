9a1c59ed419a29584a3e5457b9581c5204718d79
jesseziros <jtziros2020@gmail.com>
2020-10-29 01:39:55 -0400

    finished schedule.html functionality, started working on schedule.js, modified all other html schedule tag to say Schedule Appointment

--- public/js/schedule.js
+++ public/js/schedule.js
@@ -1,17 +1,51 @@
-// const mysql = require('mysql');
+const log = (msg) => console.log(msg)
 
-// const log = (msg) => console.log(msg)
+$(document).ready(() => {
+   const criteria1 = $("input#search1");
+   const criteria2 = $("input#search2");
+   const criteria3 = $("input#search3");
+   const criteria4 = $("input#search4");
+   const criteria5 = $("input#search5");
+   const criteria6 = $("input#search6");
+   const criteria7 = $("input#search7");
 
-// const connection = mysql.createConnection({
-//     host: 'localhost',
-//     PORT: 3306,
-//     user: 'root',
-//     password: "root",
-//     database: 'passportdb'
-// });
+    log(criteria1);
 
-// connection.connect((err, res) => {
-//     if (err) throw err;
-// })
+    $("#saveAppointment").on("click", (event) => {
+        event.preventDefault();
+        log("click event operational")
+        const appData = {
+            input1:
+            input2:
+            input3:
+            input4:
+            input5:
+            input6:
+            input7:
+        }
+    })
 
-// log('Its working')
+})
+ // const mysql = require('mysql');
+
+    // const log = (msg) => console.log(msg)
+
+
+    // const connection = mysql.createConnection({
+    //     host: 'localhost',
+    //     PORT: 3306,
+    //     user: 'root',
+    //     password: "root",
+    //     database: 'passportdb'
+    // });
+
+    // connection.connect((err, res) => {
+    //     if (err) throw err;
+    //     appScheduler();
+    // })
+
+    // const appScheduler = () => {
+
+    // }
+
+    // log('Its working')
