58e640db5734cdc53616fa7a14e954c34713f963
jesseziros <jtziros2020@gmail.com>
2020-10-29 21:27:51 -0400

    Finished scheduled appointment functionality, communicating with back end and avaiable to front, modified user.js to associate it with the scheudle.js, rename js folder, schedule.js to appointment.js, created post route for schedule functionality and changed force: true to force: false, for anyone puling they will need to use force: true to add the scheudle table then change it back to false after

---
+++ models/schedule.js
@@ -0,0 +1,40 @@
+module.exports = function (sequelize, DataTypes) {
+    const Schedule = sequelize.define("Schedule", {
+  
+      firstName: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      lastName: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      streetAddress: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      city: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      state: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      zipCode: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
+      appTime: {
+        type: DataTypes.STRING,
+        allowNull: true
+      }
+    });
+    Schedule.associate = function(models) {
+        Schedule.belongsTo(models.User, {
+          onDelete: "cascade"
+        });
+      };
+    return Schedule;
+  };
+  
